93daa27c3241d2c6e6f0362c3a8469ddd2386a0c
Rob Upchurch <rob@axialabs.com>
2021-08-25 21:54:26 -0600

    add in listings

--- components/marketplace/ListingArchive.js
+++ components/marketplace/ListingArchive.js
@@ -1,24 +1,19 @@
 import useListingsQuery from "data/queries/useListingQuery";
+import ListingCard from "./ListingCard";
 
 export default function ListingArchive(rows) {
   const { data: listings, isLoading, isError } = useListingsQuery();
 
   if (isLoading) return <div>Loading Wave</div>;
   if (isError) return <div>Error Screen</div>;
-
   console.log(listings);
-
   return (
     <div className="max-w-7xl mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-      {/* {Object.keys(listings).map((key) => (
-          <div key={key} className="l-grid__item">
-            <Listing
-              listing={listings[key]}
-              index={listings[key].propertyId - 1}
-              setNotify={setNotify}
-            />
-          </div>
-        ))} */}
+      {listings.map((listing) => (
+        <div key={listing.propertyId} className="l-grid__item">
+          <ListingCard listing={listing} />
+        </div>
+      ))}
     </div>
   );
 }
--- data/queries/useListingQuery.js
+++ data/queries/useListingQuery.js
@@ -1,12 +1,9 @@
+import { data } from "autoprefixer";
 import { useQuery } from "react-query";
 import ApiPropertyService from "../services/property.service.ts";
 
 export const QUERY_KEY = "LISTINGS";
-
 export default function useListingsQuery() {
   const { getAssets } = new ApiPropertyService();
-
-  return useQuery(QUERY_KEY, async () => await getAssets().data, {
-    staleTime: 10 * 1000,
-  });
+  return useQuery(QUERY_KEY, async () => (await getAssets()).data, {});
 }
