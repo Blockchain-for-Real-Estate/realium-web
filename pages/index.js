d77ac885274670d4f7c44e2649d95cf039bee2c7
Rob Upchurch <rob@axialabs.com>
2021-09-27 11:56:35 -0600

    remove unnecessary ssr

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,6 @@
 import HomePage from "pages/HomePage";
-import { dehydrate, QueryClient } from "react-query";
 
 const Page = () => <HomePage />;
-
-export async function getStaticProps() {
-  const queryClient = new QueryClient();
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-}
 
 Page.title = null;
 Page.description = null;
--- pages/marketplace/[propertyId].js
+++ pages/marketplace/[propertyId].js
@@ -1,14 +1,4 @@
 import PropertySingle from "pages/PropertySingle";
-import { dehydrate, QueryClient } from "react-query";
-
-export async function getStaticProps() {
-  const queryClient = new QueryClient();
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-}
 
 const Page = () => <PropertySingle />;
 
