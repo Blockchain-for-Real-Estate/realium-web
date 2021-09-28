f8b309f1641330e233a935e01756d41bc4aa8da8
Rob Upchurch <rob@axialabs.com>
2021-09-27 20:21:26 -0600

    Update Authentication to handle resirect better

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,15 @@
 import SigninPage from "pages/auth/SigninPage";
+import { getSession } from "next-auth/client";
 
-const Page = () => <SigninPage />;
+const Page = (props) => <SigninPage {...props} />;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return { props: { session } };
+}
 
 Page.title = null;
 Page.description = null;
 Page.restricted = false;
 Page.layout = "";
-export default Page;
+export default SigninPage;
