b7dffb8f7e802977e791513450da8085c19c727e
Jason Stewart <jason.iowa@gmail.com>
2021-12-01 17:55:01 -0700

    sell functionality

--- pages/api/tx/sell.js
+++ pages/api/tx/sell.js
@@ -3,6 +3,10 @@ import GetUserWallet from "server/actions/GetUserWallet";
 import GetWallet from "server/actions/GetWallet";
 import DefaultHandler from "server/DefaultHandler";
 import OfferModel from "server/models/OfferModel";
+import PropertyModel from "server/models/PropertyModel";
+import GetSignerConnectedSmartContract from "server/actions/GetSignerConnectedSmartContract";
+import { ethers, BigNumber } from "ethers";
+
 
 // REQUIRED ON ANY ROUTES WITH AUTH
 AmplifyInit();
@@ -10,6 +14,11 @@ AmplifyInit();
 export const SellOffer = async (req, res, user) => {
   const { propertyId, offerId } = req.body;
   const offer = await OfferModel.get({ propertyId, offerId });
+  const property = await PropertyModel.get(offer.propertyId)
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.NEXT_PUBLIC_AVALANCHE_API_URL
+  );
+  const total = offer?.quantity * offer?.price;
 
   if (!offer) throw Error("This offer is no longer available");
 
@@ -17,13 +26,30 @@ export const SellOffer = async (req, res, user) => {
   const buyerAddress = offer.buyerAddress;
 
   const sellerWallet = await GetUserWallet(user, true);
-  // TODO: CHECK TO MAKE SURE SELLER HAS TOKEN;
-  const buyerWallet = await GetWallet(buyerAddress, true)[0];
-  // TODO: CHECK TO MAKE SURE BUYER HAS SUFFICENT AVAX
 
-  // TODO: TRANSACT AVAX from buyer wallet to sellerWallet
-  
-  // TODO: TRANSACT TOKEN FROM sellerWallet to buyer address
+  const buyerWallet = await GetWallet(buyerAddress, true);
+
+  //Connect wallet to provider
+  let smartContract = await GetSignerConnectedSmartContract(sellerWallet.privateKey, provider, property);
+
+  // CHECK TO MAKE SURE BUYER HAS SUFFICENT AVAX
+  const avaxBalance = await provider.getBalance(buyerWallet[0].address);
+  if (ethers.utils.formatEther(avaxBalance) < total){
+    throw Error;
+  }
+  // CHECK TO MAKE SURE SELLER HAS TOKEN;
+  const tokenBalanceBigNum = await smartContract.balanceOf(sellerWallet.address);
+  const tokenBalance = tokenBalanceBigNum.toNumber();
+  if (tokenBalance < offer.quantity){
+    throw Error;
+  }
+
+  console.log("increase")
+  // TRANSACT AVAX from buyer wallet to sellerWallet and TRANSACT TOKEN FROM sellerWallet to buyer address
+  const increaseAllowanceResponse = await smartContract.increaseAllowance(buyerWallet[0].address, offer.quantity);
+  smartContract = await GetSignerConnectedSmartContract(sellerWallet.privateKey, provider, property);
+  const sale = await smartContract.sale(sellerWallet.address, offer.quantity, offer.price, {value: ethers.utils.parseEther(total.toString()), gasLimit: 2500000 });
+  const response = await sale.wait();
 
   await OfferModel.delete({ propertyId, offerId });
   return res.send(offer);
